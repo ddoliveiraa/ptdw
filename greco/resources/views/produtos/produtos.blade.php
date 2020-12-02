b4c3faf7e1d81dbe50e59ce8e93b1d9478b01d73
b.ferreira <b.ferreira@ua.pt>
2020-12-02 17:05:08 +0000

    add produtos

--- greco/resources/views/produtos/produtos.blade.php
+++ greco/resources/views/produtos/produtos.blade.php
@@ -5,13 +5,10 @@
     <link rel="stylesheet" href="../../plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
     <link rel="stylesheet" href="../../plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
     <link rel="stylesheet" href="../../plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
-<<<<<<< HEAD
     <!-- Theme style -->
     <link rel="stylesheet" href="../../dist/css/adminlte.min.css">
     
     
-=======
->>>>>>> fede82f4289027efeca096906e24f743841f934d
 @endsection
 
 @section('content')
