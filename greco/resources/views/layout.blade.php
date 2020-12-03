bec926880b1aecf7f840073c38a6258b1f7ace6e
TDHOzzyMania <33217701+TDHOzzyMania@users.noreply.github.com>
2020-12-03 15:51:06 +0000

    Fornecedores

--- greco/resources/views/layout.blade.php
+++ greco/resources/views/layout.blade.php
@@ -187,6 +187,14 @@
                             </a>
                         </li>
                         <li class="nav-item">
+                            <a href="/operadores" class="nav-link {{ Request::is('operadores') ? 'active' : '' }}">
+                                <i class="nav-icon fas fa-truck"></i>
+                                <p>
+                                    Fornecedores
+                                </p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
                             <a href="/armazem" class="nav-link {{ Request::is('armazem') ? 'active' : '' }}">
                                 <i class="nav-icon fas fa-warehouse"></i>
                                 <p>
