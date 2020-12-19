a6777d5b5122c1fe10844383e7ac9fc5b82e13e0
mqn-m <57487639+mqn-m@users.noreply.github.com>
2020-12-19 16:39:46 +0000

    Update AppServiceProvider.php

--- greco/app/Providers/AppServiceProvider.php
+++ greco/app/Providers/AppServiceProvider.php
@@ -15,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
     {
         $this->app->bind('path.public', function() {
              return '/~ptdw-2020-gr3';
-            // return '';
+            //return '';
           });
     }
 
