cf92388d5fbc39136abb512e0ebd4f488882166c
TDHOzzyMania <33217701+TDHOzzyMania@users.noreply.github.com>
2020-12-19 16:48:40 +0000

    aaa

--- greco/resources/views/movimentos/historico.blade.php
+++ greco/resources/views/movimentos/historico.blade.php
@@ -252,7 +252,7 @@
 
             //criação e inserção da combobox movimentos dentro da div da datatable
             var movimentos = $("<select></select>").attr('id', 'movimento');
-            movimentos.addClass('col-md-1');
+            movimentos.addClass('col-md-1 form-control select');
             $('div.toolbar').append(movimentos);
             $('#movimento').append(new Option("{{ __('lang.movimento') }}",
                 "Entradas e Saídas"));
