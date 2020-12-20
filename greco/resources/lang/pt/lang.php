99d201a861af775fc190fecb8ba457e6b1e103d8
TDHOzzyMania <33217701+TDHOzzyMania@users.noreply.github.com>
2020-12-20 16:47:44 +0000

    Update lang.php

--- greco/resources/lang/pt/lang.php
+++ greco/resources/lang/pt/lang.php
@@ -170,6 +170,6 @@ return [
     'locale-date' => 'pt',
 
     /*Datatable lang */
-    'url-lang-dt' => "/plugins/datatable_portuguese.json"
+    'url-lang-dt' => "//cdn.datatables.net/plug-ins/1.10.22/i18n/Portuguese.json"
 
 ];
