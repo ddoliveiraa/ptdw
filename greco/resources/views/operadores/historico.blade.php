1af1f5fa6cc680cd3624c71c87f6c500ae9c22e8
mqn-m <57487639+mqn-m@users.noreply.github.com>
2020-12-29 14:16:57 +0000

    Update historico.blade.php

--- greco/resources/views/operadores/historico.blade.php
+++ greco/resources/views/operadores/historico.blade.php
@@ -243,7 +243,6 @@
                             columns: ':visible:not(:last-child)'
                         }
                     },
-                    'colvis'
                 ],
                 "initComplete": function() {
                     table.buttons().container().appendTo('div.toolbar');
