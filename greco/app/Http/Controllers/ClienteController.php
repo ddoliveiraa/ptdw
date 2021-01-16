e50407851abd4560f7342747f0f1e9a50defd987
TDHOzzyMania <33217701+TDHOzzyMania@users.noreply.github.com>
2021-01-16 11:33:02 +0000

    Cliente restful

--- greco/app/Http/Controllers/ClienteController.php
+++ greco/app/Http/Controllers/ClienteController.php
@@ -3,8 +3,78 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Cliente;
 
 class ClienteController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('clientes.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Cliente  $cliente
+     * @return \Illuminate\Http\Response
+     */
+
+    public function show(Cliente $cliente)
+    {
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store()
+    {
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Cliente  $fornecedor
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Cliente $cliente)
+    {
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Cliente  $cliente
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Cliente $cliente)
+    {
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Cliente  $cliente
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Cliente $cliente)
+    {
+    }
 }
