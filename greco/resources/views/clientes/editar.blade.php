ab4537d1718ec61f25fae718ae37909c27b2f86a
carolinatavares <57751421+carolinatavares@users.noreply.github.com>
2021-01-24 00:34:39 +0000

    clientes meu mehhh :-1:

--- greco/resources/views/clientes/editar.blade.php
+++ greco/resources/views/clientes/editar.blade.php
@@ -5,6 +5,7 @@
     <!-- Select2 -->
     <link rel="stylesheet" href="{{ public_path() }}/plugins/select2/css/select2.min.css">
     <link rel="stylesheet" href="{{ public_path() }}/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
+    <link rel="stylesheet" href="{{ public_path() }}/dist/css/toastr.css"/>
 
 @endsection
 
@@ -19,7 +20,7 @@
                         <li class="breadcrumb-item"><a href="{{ public_path() }}/welcome">Home</a></li>
                         <li class="breadcrumb-item"><a href="{{ public_path() }}/clientes">{{ __('lang.clientes') }}</a>
                         </li>
-                        <li class="breadcrumb-item active">{{ __('lang.editar') }} {{ __('lang.cliente') }}</li>
+                        <li class="breadcrumb-item active">{{$cliente->designacao}}</li>
                     </ol>
                 </div>
             </div>
@@ -41,14 +42,17 @@
                     <!-- general form elements -->
                     <div class="card card-primary">
                         <!-- form start -->
-                        <form>
+                        <form method="POST" action="/clientes/editado/{{$cliente->id}}">
+                        @csrf
+                        @method('PUT') 
                             <div class="card-body">
+                            <input type="hidden" id="id" name = "id" value="{{$cliente->id}}">
                                 <div class="row">
                                     <div class="col-md-12">
                                         <div class="form-group">
                                             <label for="designacao">{{ __('lang.designacao') }}</label>
                                             <div class="input-group">
-                                                <input type="text" class="form-control" id="designacao" value="Departamento de Biologia">
+                                                <input type="text" class="form-control" id="designacao" name="designacao" value="{{$cliente->designacao}}">
                                             </div>
                                         </div>
                                     </div>
@@ -57,13 +61,10 @@
                                     <div class="col-md-12">
                                         <div class="form-group">
                                             <label>{{ __('lang.responsavel') }}</label>
-                                            <select class="select2" multiple="multiple" id="responsavel"
-                                                data-placeholder="{{ __('lang.selecione o') }} {{ __('lang.responsavel') }}"
-                                                style="width: 100%;">
-                                                <option selected>Carolina Tavares | carol@ua.pt</option>
-                                                <option>Diogo Oliveira | diogo@ua.pt</option>
-                                                <option>Maria Nobre | maria@ua.pt</option>
-                                                <option>Bruno Ferreira | bruno@ua.pt</option>
+                                            <select class="select2" multiple="multiple" id="responsavel" name="responsavel" style="width: 100%;">
+                                            @foreach ($responsaveis as $r)
+                                            <option value="{{$r->id}}" @foreach($responsaveis as $res) @if($res->id == $r->id) selected @endif @endforeach>{{$r->nome}} | {{$r->email}}</option>
+                                                @endforeach
                                             </select>
                                         </div>
                                     </div>
@@ -72,13 +73,10 @@
                                     <div class="col-md-12">
                                         <div class="form-group">
                                             <label>{{ __('lang.solicitante') }}s</label>
-                                            <select class="select2" multiple="multiple" id="solicitante"
-                                                data-placeholder="{{ __('lang.selecione o') }} {{ __('lang.solicitante') }}"
-                                                style="width: 100%;">
-                                                <option>Carolina Tavares | carol@ua.pt</option>
-                                                <option selected>Diogo Oliveira | diogo@ua.pt</option>
-                                                <option selected>Maria Nobre | maria@ua.pt</option>
-                                                <option>Bruno Ferreira | bruno@ua.pt</option>
+                                            <select class="select2" multiple="multiple" id="solicitante" name="solicitante" style="width: 100%;">
+                                            @foreach ($solicitantes as $s)
+                                            <option value="{{$s->id}}"  @foreach($solicitantes as $sol) @if($sol->id == $s->id) selected @endif @endforeach>{{$s->nome}} | {{$s->email}}</option>
+                                                @endforeach
                                             </select>
                                         </div>
                                     </div>
@@ -87,7 +85,7 @@
 
                             <div class="form-group">
                                 <label for="obvs">{{ __('lang.observacoes') }}</label>
-                                <textarea id="obvs" class="form-control" rows="4"></textarea>
+                                <textarea id="obvs" name="obvs" class="form-control" rows="4">{{$cliente->obs}}</textarea>
                             </div>
                         </div>
                     
@@ -95,12 +93,12 @@
                     <div class="card-footer">
                         <div class="row">
                             <div class="col-md-3">
-                                <a href="{{ public_path() }}/clientes/show" role="button"
+                                <a href="{{ public_path() }}/clientes/{{$cliente->id}}" role="button"
                                     class="btn btn-block btn-default">{{ __('lang.cancelar') }}</a>
                             </div>
                             <div class="ml-auto col-3">
-                                <a href="{{ public_path() }}/clientes/show" role="button"
-                                    class="btn btn-block btn-secondary">{{ __('lang.guardar') }}</a>
+                            <button id="guardar" type="submit"
+                                class="btn btn-block btn-secondary" tabindex="12">{{ __('lang.guardar') }}</button>
                             </div>
                         </div>
                     </div>
@@ -119,8 +117,27 @@
 
     <!-- Select2 -->
     <script src="{{ public_path() }}/plugins/select2/js/select2.full.min.js"></script>
+    <!-- Toastr -->
+    <script src="{{ public_path() }}/dist/js/toastr.min.js"></script>
 
     <script>
+    $(function() {
+            if('{{ $errors->count() > 0}}') {
+                toastr["error"]("Por favor reveja os campos.", "Erro ao editar Cliente")
+            }
+    })
+
+    $('#responsaveis').tagEditor();
+        $('#solicitantes').tagEditor();
+
+        $('#selecionarResponsavel').on('click', function() {
+            $('#responsaveis').tagEditor('addTag', $('#responsavel_nome').val()+" | "+$('#responsavel_email').val());
+        });
+
+        $('#selecionarSolicitante').on('click', function() {
+            $('#solicitantes').tagEditor('addTag', $('#solicitante_nome').val()+" | "+$('#solicitante_email').val());
+        });
+
         $(function() {
             //Initialize Select2 Elements
             $('.select2').select2()
