935ed5edcd0761f727f843290b566c0f182f9e24
TDHOzzyMania <austin.foote@ua.pt>
2021-01-25 21:40:33 +0000

    Update historico.blade.php

--- greco/resources/views/movimentos/historico.blade.php
+++ greco/resources/views/movimentos/historico.blade.php
@@ -37,100 +37,101 @@
 
     <section class="content">
         <div class="container-fluid">
-        </div>
-        <div class="row">
-            <div class="col-md-2">
-                <div class="card card-dark">
-                    <div class="card-header">
-                        <h3 class="card-title text-light">Filtros</h3>
-                    </div>
-                    <div class="card-body sup">
-                        <div class="filtros">
-                            <select id="familia" class="col-md-12 form-control select">
-                                <option value="Familia">{{ __('lang.familia') }}</option>
-                                <option value="Químico">{{ __('lang.quimicos') }}</option>
-                                <option value="Não Químico">{{ __('lang.nao quimicos') }}</option>
-                            </select>
-                            <select id="sub-familia" class="col-md-12 form-control select" style="display: none;">
-                                <option value="Sub-Familia">{{ __('lang.sub-familia') }}</option>
-                                @foreach ($subfamilias as $subfamilia)
-                                    <option value="{{ $subfamilia->nome }}">{{ $subfamilia->nome }}</option>
-                                @endforeach
-                            </select>
-                            <input type="text" class="col-md-12 form-control" id="intervalo">
-                            <button id="pictogramas"
-                                class="col-md-12 btn btn-secondary">{{ __('lang.pictograma') }}s</button>
-                            <button id="filter" class="btn btn-danger btn-block">Reset</button>
+        
+            <div class="row">
+                <div class="col-md-2">
+                    <div class="card card-dark">
+                        <div class="card-header">
+                            <h3 class="card-title text-light">Filtros</h3>
                         </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-10">
-                <ul class="nav nav-tabs" id="addProdutosTab" role="tablist">
-                    <li class="nav-item">
-                        <a class="nav-link active" id="entrada-tab" data-toggle="tab" href="#entrada" role="tab"
-                            aria-controls="entrada" aria-selected="true">{{ __('lang.entrada') }}s</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" id="saida-tab" data-toggle="tab" href="#saida" role="tab" aria-controls="saida"
-                            aria-selected="false"> {{ __('lang.saida') }}s</a>
-                    </li>
-                </ul>
-                <!-- tab-content -->
-                <div class="tab-content" id="MovimentosTabs">
-                    <div class="tab-pane fade show active" id="entrada" role="tabpanel" aria-labelledby="entrada-tab">
-                        <div class="card">
-                            <!-- /.card-header -->
-                            <div class="card-body">
-                                <table id="entradas" class="table table-bordered table-striped">
-                                    <thead class="bg-dark">
-                                        <tr>
-                                            <th>{{ __('lang.produto') }}</th>
-                                            <th>{{ __('lang.n-embalagem') }}</th>
-                                            <th>{{ __('lang.localização') }}</th>
-                                            <th>{{ __('lang.embalagem') }}</th>
-                                            <th>{{ __('lang.fornecedor') }}</th>
-                                            <th>{{ __('lang.data de entrada') }}</th>
-                                            <th>{{ __('lang.data de validade') }}</th>
-                                            <th>{{ __('lang.data de termino') }}</th>
-                                            <th>{{ __('lang.operador') }}</th>
-                                            <th>{{ __('lang.familia') }}</th>
-                                            <th>{{ __('lang.sub-familia') }}</th>
-                                            <th></th>
-                                        </tr>
-                                    </thead>
-                                </table>
-                                <!-- /.card-body -->
-                            </div>
-                            <!-- /.card -->
-                        </div>
-                    </div>
-                    <div class="tab-pane fade" id="saida" role="tabpanel" aria-labelledby="saida-tab">
-                        <div class="card">
-                            <!-- /.card-header -->
-                            <div class="card-body">
-                                <table id="saidas" class="table table-bordered table-striped">
-                                    <thead class="bg-dark">
-                                        <tr>
-                                            <th>{{ __('lang.produto') }}</th>
-                                            <th>{{ __('lang.n-embalagem') }}</th>
-                                            <th>{{ __('lang.cliente') }}</th>
-                                            <th>{{ __('lang.solicitante') }}</th>
-                                            <th>{{ __('lang.operador') }}</th>
-                                            <th>{{ __('lang.data') }}</th>
-                                            <th>{{ __('lang.familia') }}</th>
-                                            <th>{{ __('lang.sub-familia') }}</th>
-                                            <th></th>
-                                        </tr>
-                                    </thead>
-                                </table>
+                        <div class="card-body sup">
+                            <div class="filtros">
+                                <select id="familia" class="col-md-12 form-control select">
+                                    <option value="Familia">{{ __('lang.familia') }}</option>
+                                    <option value="Químico">{{ __('lang.quimicos') }}</option>
+                                    <option value="Não Químico">{{ __('lang.nao quimicos') }}</option>
+                                </select>
+                                <select id="sub-familia" class="col-md-12 form-control select" style="display: none;">
+                                    <option value="Sub-Familia">{{ __('lang.sub-familia') }}</option>
+                                    @foreach ($subfamilias as $subfamilia)
+                                        <option value="{{ $subfamilia->nome }}">{{ $subfamilia->nome }}</option>
+                                    @endforeach
+                                </select>
+                                <input type="text" class="col-md-12 form-control" id="intervalo">
+                                <button id="pictogramas"
+                                    class="col-md-12 btn btn-secondary">{{ __('lang.pictograma') }}s</button>
+                                <button id="filter" class="btn btn-danger btn-block">Reset</button>
                             </div>
                         </div>
                     </div>
                 </div>
-                <!-- /.col -->
+                <div class="col-md-10">
+                    <ul class="nav nav-tabs" id="addProdutosTab" role="tablist">
+                        <li class="nav-item">
+                            <a class="nav-link active" id="entrada-tab" data-toggle="tab" href="#entrada" role="tab"
+                                aria-controls="entrada" aria-selected="true">{{ __('lang.entrada') }}s</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" id="saida-tab" data-toggle="tab" href="#saida" role="tab" aria-controls="saida"
+                                aria-selected="false"> {{ __('lang.saida') }}s</a>
+                        </li>
+                    </ul>
+                    <!-- tab-content -->
+                    <div class="tab-content" id="MovimentosTabs">
+                        <div class="tab-pane fade show active" id="entrada" role="tabpanel" aria-labelledby="entrada-tab">
+                            <div class="card">
+                                <!-- /.card-header -->
+                                <div class="card-body">
+                                    <table id="entradas" class="table table-bordered table-striped">
+                                        <thead class="bg-dark">
+                                            <tr>
+                                                <th>{{ __('lang.produto') }}</th>
+                                                <th>{{ __('lang.n-embalagem') }}</th>
+                                                <th>{{ __('lang.localização') }}</th>
+                                                <th>{{ __('lang.embalagem') }}</th>
+                                                <th>{{ __('lang.fornecedor') }}</th>
+                                                <th>{{ __('lang.data de entrada') }}</th>
+                                                <th>{{ __('lang.data de validade') }}</th>
+                                                <th>{{ __('lang.data de termino') }}</th>
+                                                <th>{{ __('lang.operador') }}</th>
+                                                <th>{{ __('lang.familia') }}</th>
+                                                <th>{{ __('lang.sub-familia') }}</th>
+                                                <th></th>
+                                            </tr>
+                                        </thead>
+                                    </table>
+                                    <!-- /.card-body -->
+                                </div>
+                                <!-- /.card -->
+                            </div>
+                        </div>
+                        <div class="tab-pane fade" id="saida" role="tabpanel" aria-labelledby="saida-tab">
+                            <div class="card">
+                                <!-- /.card-header -->
+                                <div class="card-body">
+                                    <table id="saidas" class="table table-bordered table-striped">
+                                        <thead class="bg-dark">
+                                            <tr>
+                                                <th>{{ __('lang.produto') }}</th>
+                                                <th>{{ __('lang.n-embalagem') }}</th>
+                                                <th>{{ __('lang.cliente') }}</th>
+                                                <th>{{ __('lang.solicitante') }}</th>
+                                                <th>{{ __('lang.operador') }}</th>
+                                                <th>{{ __('lang.data') }}</th>
+                                                <th>{{ __('lang.familia') }}</th>
+                                                <th>{{ __('lang.sub-familia') }}</th>
+                                                <th></th>
+                                            </tr>
+                                        </thead>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /.col -->
+                </div>
+                <!-- /.row -->
             </div>
-            <!-- /.row -->
         </div>
         <!-- /.container-fluid -->
 
@@ -175,6 +176,7 @@
                 <!-- /.modal-content -->
             </div>
         </div>
+        
     </section>
     <!-- /.content -->
 
